852c66b68c7ae4db21f221b8e773d5aa1156b83f
mihaidurus <mihai.durus@gmail.com>
2021-04-05 18:10:27 +0300

    Ezvax_login_register

---
+++ EZVax/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using EZVax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EZVax.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(EZVax.Models.Users userModel)
+        {
+            using (HospitalEntities db = new HospitalEntities())
+            {
+                var userDetails = db.Users.Where(x => x.CNP  == userModel.CNP && x.password == userModel.password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong CNP or password.";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.id;
+                    Session["userName"] = userDetails.nume;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View();
+        }
+        public ActionResult LogOut()
+        {
+            int userId = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
