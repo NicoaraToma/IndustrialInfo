089486f720fb665e1bb1317fd1ccde940243b257
mihaidurus <mihai.durus@gmail.com>
2021-04-19 14:16:47 +0300

    login+register

---
+++ Project_II/ezvax/Controllers/ProfileController.cs
@@ -0,0 +1,19 @@
+﻿using ezvax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ezvax.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Profile (int id=0)
+        {
+            ProfilMedical medicalProfile = new ProfilMedical();
+            return View(medicalProfile);
+        }
+    }
+}
---
+++ Project_II/ezvax/Controllers/RegistrationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ezvax.Models;
+using System.Web.Mvc;
+
+namespace ezvax.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult Register()
+        {
+            Users userModel = new Users();
+            return View(userModel);
+        }
+        [HttpPost]
+        public ActionResult Register(Users userModel)
+        {
+            using (HospitalEntities dbModel = new HospitalEntities())
+            {
+                if (dbModel.Users.Any(x => x.CNP == userModel.CNP))
+                {
+                    ViewBag.DuplicateMessage = "CNP already registered.";
+                    return View("Register", userModel);
+                }
+
+                dbModel.Users.Add(userModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful.";
+            return View("Register", new Users());
+        }
+    }
+}
